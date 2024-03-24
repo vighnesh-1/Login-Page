9dfff7ba680a865de70367a5620940fb54834595
Vighnesh Ch <vighnesh8074849951@gmail.com>
2024-03-24 19:47:56 +0530

    first commit

---
+++ frontend/src/assets/components/Footer.jsx
@@ -0,0 +1,30 @@
+
+export default function Footer() {
+    
+    return (
+        <div style={{padding: "12px 0px", backgroundColor : "rgb(232, 227, 227)", textAlign: 'center', position:'fixed', width: '100%',height : '85px', bottom : '0', left: '0', right: '0', zIndex : '999'}}>
+            
+            <div className="text-center">
+                    <h5>
+                        Made with by Vighnesh Ch
+                    </h5>
+            </div> 
+
+            <div className="text-center pt-1">
+                <a href="https://www.linkedin.com/in/vighnesh3b207/" target="_blank" rel="noreferrer">
+                    <i className="bi bi-linkedin mx-2" style={{fontSize : "20px"}}></i>
+                </a>
+                
+                <a href="https://github.com/vighnesh-1" target="_blank" rel="noreferrer">
+                    <i className="bi bi-github mx-2" style={{fontSize : "21px"}}></i>
+                </a>
+
+                <a href="mailto:akshataganbote61843@gmail.com" target="_blank" rel="noreferrer">
+                    <i className="bi bi-envelope-fill mx-2" style={{fontSize : "21px"}}></i>
+                </a>
+            </div>
+            {/* <footer style={{backgroundColor: 'crimson', color:'springgreen', textAlign: 'center', position:'fixed', width: '100%',height : '50px', bottom : '0', left: '0', right: '0', zIndex : '999'}}><h1>THIS IS MY FOOTER</h1></footer> */}
+        </div>
+       
+    )
+}
---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './assets/components/App.jsx'
+import Footer from './assets/components/Footer.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+    <Footer/>
+  </React.StrictMode>
+)
